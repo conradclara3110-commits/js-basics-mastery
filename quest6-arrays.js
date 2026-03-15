d4b7ba93c80883a02964ead96f86c546ab041225
Clara Millinga <conradclara3110@gmail.com>
2026-03-15 14:13:31 +0300

    Add initial HTML structure to quest6-arrays.js

---
+++ quest6-arrays.js
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Quest 6</title>
+</head>
+<body>
+
+<h1 id="title">Original Title</h1>
+
+<p id="message">Old paragraph text</p>
+
+<script src="quest6-arrays.js"></script>
+
+</body>
+</html>
