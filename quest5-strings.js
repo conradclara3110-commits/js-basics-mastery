a9dcd8dc9dd621bdb01025a001385cbfc7766d54
Clara Millinga <conradclara3110@gmail.com>
2026-03-15 14:10:53 +0300

    Add HTML structure to quest5-strings.js

---
+++ quest5-strings.js
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Quest 5</title>
+</head>
+<body>
+
+<h1 id="title">Original Title</h1>
+
+<p class="text">Paragraph One</p>
+<p class="text">Paragraph Two</p>
+
+<script src="quest5-strings.js"></script>
+
+</body>
+</html>
