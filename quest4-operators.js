a87e18470d76ed0ac975f550da914a0db8a9e1a8
Clara Millinga <conradclara3110@gmail.com>
2026-03-15 14:00:28 +0300

    Add quest4-operators.js with variable examples

---
+++ quest4-operators.js
@@ -0,0 +1,16 @@
+let score=42;
+let score="Hello!";
+let isLoggedIn=true;
+
+let newScore=score+10;
+console.log('New score: ${newScore}');
+
+console.log(typeof score);
+console.log(typeof message);
+console.log(typeof isLoggedIn);
+
+let greeting="Hello," + "World!";
+console.log(greeting);
+
+let name="Alex";
+console.log('Hello, ${name}!');
